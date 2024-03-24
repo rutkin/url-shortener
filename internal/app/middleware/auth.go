6d334239fc1b353a21b7e2660cc80e8b9d2c125e
rutkin <r.utkin91@gmail.com>
2024-03-24 19:03:40 +0300

    add logging

--- internal/app/middleware/auth.go
+++ internal/app/middleware/auth.go
@@ -86,6 +86,7 @@ func WithUserID(h http.Handler) http.Handler {
 			logger.Log.Error("failed to set user id", zap.String("error", err.Error()))
 			w.WriteHeader(http.StatusInternalServerError)
 		}
+		logger.Log.Info("with userID", zap.String("userID", userID))
 		ctx := context.WithValue(r.Context(), service.UserIDKey, userID)
 		h.ServeHTTP(w, r.WithContext(ctx))
 	}
